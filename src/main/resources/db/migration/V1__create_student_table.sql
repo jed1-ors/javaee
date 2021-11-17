eb376176cc7af05531744ff457b254153ea02353
mikhail_mikhailov <mikhail_mikhaylov@senla.eu>
2021-11-17 16:28:12 +0300

    fix of running

--- src/main/resources/db/migration/V1__create_student_table.sql
+++ src/main/resources/db/migration/V1__create_student_table.sql
@@ -5,3 +5,6 @@ create table student (
     middle_name varchar not null,
     birthday date not null
 );
+
+insert into student (first_name, last_name, middle_name, birthday)
+values ('Ivan', 'Ivanov', 'Ivanovich', '1991-10-10');
